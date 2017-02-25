2b4a5215b7b233ee7d8ac1b793411a5bd7863751
Angela Sanecki <ags425@kellogg.local>
2017-02-24 21:13:23 -0600

    uploading

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+
+   
+
+
+
+
+//If the total score equals the random number, the user wins and gets one point added to the win number and the game is reset - the random number is reset to a different number, the total is reset to zero and the random number that belongs to each of the crystals is reset to another random number
+
+
+
+//If the total score goes over hte random number that is generated, the "losses" number increases by 1 and the game is reset - the random number is reset to a different number, the total is reset to zero, and the random number that belong to each of the crystals is reset to another random number 
+
+    
+
